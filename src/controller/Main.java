aec7cfa9b9481c1a81bcf126f9ab7bd8ca06173b
Chaitra6 <48185350+Chaitra6@users.noreply.github.com>
2020-09-08 12:22:13 +0530

    FSJ0820A003

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -4,10 +4,21 @@ import java.sql.Connection;
 
 import utility.ConnectionManager;
 
+
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
-	// Fill your code
+		Connection conn = null;	
+		ConnectionManager cm = new ConnectionManager();
+		conn = cm.getConnection();
+		
+		if(conn!=null) {
+			System.out.println("Connection Established.");
+		}
+		else {
+			System.out.println("Check your Connection");
+		}
+		
 		
 	}
 }
